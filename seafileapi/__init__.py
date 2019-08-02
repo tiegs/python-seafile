bfdda1b634845116717b4dfef558829f1c587859
i.rumyantsev <igorrum@mail.ru>
2019-08-02 18:43:43 +0300

    fix bug

--- seafileapi/__init__.py
+++ seafileapi/__init__.py
@@ -1,5 +1,5 @@
 from seafileapi.client import SeafileApiClient
 
-def connect(server, username, password, token):
+def connect(server, username, password, token=None):
     client = SeafileApiClient(server, username, password, token)
     return client
